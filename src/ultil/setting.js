9824f0129134b6256ddf336335abc79912a2ee23
Truong <nguyenvantruong9876>
2022-11-05 19:36:41 +0700

    fix

--- src/ultil/setting.js
+++ src/ultil/setting.js
@@ -7,5 +7,5 @@ export const USER_LOGIN = "USER_LOGIN";
 export const GROUP_ID = "GP08";
 
 export const DOMAIN = "https://movienew.cybersoft.edu.vn";
-export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwMyIsIkhldEhhblN0cmluZyI6IjI3LzAxLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3NDc3NzYwMDAwMCIsIm5iZiI6MTY0NTgwODQwMCwiZXhwIjoxNjc0OTI1MjAwfQ.LrAtwphIhIIrGz5ssg4cQOfyHiF8X8oDbkMMa8YAebQ";
+export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwNCIsIkhldEhhblN0cmluZyI6IjA0LzA0LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY4MDU2NjQwMDAwMCIsIm5iZiI6MTY1MjYzNDAwMCwiZXhwIjoxNjgwNzE0MDAwfQ.XE8uuSrZpBccfaHjIzdPXm7QejmcAjIWEkG8333amBU";
 export const ACCESS_TOKEN = "accessToken";
